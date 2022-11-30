0a79988ec915dec71f1e401444ab91c2a616f846
Siyabonga Fortune <siyabonga@techsprung.co.za>
2022-11-30 19:17:17 +0200

    added homepage link to logo

--- components/logo.inc.php
+++ components/logo.inc.php
@@ -1,4 +1,6 @@
 <div class="logo">
-    <img class="logo-img" src="images/icons/logo1.png" alt="logo image">
-    <h1 class="logo-title" >get nerfed!</h1>
+    <a class="logo-link" href="index.php">
+        <img class="logo-img" src="images/icons/logo1.png" alt="logo image">
+        <h1 class="logo-title" >get nerfed!</h1>
+    </a>
 </div>
