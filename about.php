62318d302753190535c24c37353faf2242409cfd
Siyabonga Fortune <siyabonga@techsprung.co.za>
2022-11-30 19:09:36 +0200

    added welcome message component

--- about.php
+++ about.php
@@ -103,41 +103,8 @@
 
     <script src="js/script.js"></script>
     
-    <script>
-        // get database connection status
-        // $db->status declared in php file
-        var connStatus = <?= json_encode($db->status) ?>;
-
-        // dispplay status
-        var welcomeMsg = document.getElementById("welcome-msg");
-        welcomeMsg.innerHTML = connStatus + "<br>";
-        welcomeMsg.style.display = "block";
-
-        setTimeout(function(){ 
-            welcomeMsg.style.display = "none";
-        }, 7000); // timeout for status message
-
-		var displayForm = <?= json_encode($displayForm) ?>;
-		if (displayForm) {
-			document.getElementById("form_container").style.display = 'block';
-		}
-
-		var isValidUser = <?= json_encode($isValidUser) ?>;
-		var username = <?= json_encode($username) ?>;
-
-		if (isValidUser) {
-            // window.location.replace("index.php"); // redirect to main page
-			document.getElementById("user_btn_text").textContent = username;
-
-			welcomeMsg.innerHTML = "Welcome " + validUsername;
-			welcomeMsg.style.display = "block";
-
-			setTimeout(function(){ 
-				welcomeMsg.style.display = "none";
-			}, 7000); // timeout for welcome message
-		}
-
-    </script>
+    <!-- Username and welcome Msg Component -->
+    <?php require("components/welcome.msg.inc.php"); ?> 
 
 </body>
 
--- contact.php
+++ contact.php
@@ -40,7 +40,7 @@
             <h1 class="section-title section-title_intro" >
                 contact us
             </h1>
-            
+
             <!-- Dropdown Theme Menu Component -->
             <?php require("components/theme.menu.inc.php"); ?> 
 
@@ -205,41 +205,8 @@
 
     <script src="js/script.js"></script>
 
-    <script>
-        // get database connection status
-        // $db->status declared in php file
-        var connStatus = <?= json_encode($db->status) ?>;
-
-        // dispplay status
-        var welcomeMsg = document.getElementById("welcome-msg");
-        welcomeMsg.innerHTML = connStatus + "<br>";
-        welcomeMsg.style.display = "block";
-
-        setTimeout(function(){ 
-            welcomeMsg.style.display = "none";
-        }, 7000); // timeout for status message
-
-		var displayForm = <?= json_encode($displayForm) ?>;
-		if (displayForm) {
-			document.getElementById("form_container").style.display = 'block';
-		}
-
-		var isValidUser = <?= json_encode($isValidUser) ?>;
-		var username = <?= json_encode($username) ?>;
-
-		if (isValidUser) {
-            // window.location.replace("index.php"); // redirect to main page
-			document.getElementById("user_btn_text").textContent = username;
-
-			welcomeMsg.innerHTML = "Welcome " + validUsername;
-			welcomeMsg.style.display = "block";
-
-			setTimeout(function(){ 
-				welcomeMsg.style.display = "none";
-			}, 7000); // timeout for welcome message
-		}
-
-    </script>
+    <!-- Username and welcome Msg Component -->
+    <?php require("components/welcome.msg.inc.php"); ?> 
 
 </body>
 
