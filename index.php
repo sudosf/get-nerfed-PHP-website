f044f1d20fbf06c390792a31ddc7e01350e9c555
Siyabonga Fortune <siyabonga@techsprung.co.za>
2022-11-30 18:22:56 +0200

    disabled database dependency

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php require('./server/app.php'); ?>
+<?php // require('./server/app.php'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -66,7 +66,7 @@
 
             <!-- Login form -->
             <div id="form_container" class="modal">
-                <form id="login_form" name="login" class="modal-content animate" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                <form id="login_form" name="login" class="modal-content animate" action="<?php // echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
                     <!-- close button -->
                     <div class="imgcontainer">
                         <span onclick="closeForm()" class="close" title="Close Modal">&times;</span>
@@ -76,10 +76,10 @@
                     <!-- input fields -->
                     <div class="input-container">
                         <label for="uname"><b>Username</b></label>
-                        <input id="username_input" type="text" placeholder="Enter Username" name="username" value="<?php echo $username; ?>">
+                        <input id="username_input" type="text" placeholder="Enter Username" name="username" value="<?php // echo $username; ?>">
 
                         <label for="psw"><b>Password</b></label>
-                        <input id="password_input" type="password" placeholder="Enter Password" name="psw" value="<?php echo $password; ?>">
+                        <input id="password_input" type="password" placeholder="Enter Password" name="psw" value="<?php // echo $password; ?>">
                             
                         <button id="login" name="submitLogin" value="Register" type="submit" >Log in</button>
                         <label>
@@ -87,7 +87,7 @@
                             Remember Me
                         </label>
 
-                        <h3 class="required-field"><?php echo $loginFormErr; ?></h3>
+                        <h3 class="required-field"><?php // echo $loginFormErr; ?></h3>
 
                         
                     </div>
